6afabba2ff80c9babe95ce06551eb52ab084d25d
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2023-06-06 21:45:33 +0530

    remove dead code

--- src/components/login/LoginButton.jsx
+++ src/components/login/LoginButton.jsx
@@ -1,9 +1,4 @@
-import { useContext } from 'react';
-import { useRouter } from "next/router";
-import AuthContext from '@/context/AuthContext';
-
 export const LoginButton = ({ handleLogin }) => {
-    const router = useRouter();
     return (
         <>
             <button
